a01e0ac47872c911cb97dab636d198e0cf098508
MohitPaudel05 <mohitpoudel39@gmail.com>
2026-05-22 07:45:19 +0545

    refactor routing in App component and update Home page button to use Link

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,32 +1,17 @@
 
-import { useEffect, useState } from 'react'
-import routes from './routes/route'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
 import Home from './pages/Home'
 import StaticForm from './pages/StaticForm'
 
-const componentMap = {
-  Home,
-  StaticForm,
-} as const
-
 const App = () => {
-  const [pathname, setPathname] = useState(() => window.location.pathname)
-
-  useEffect(() => {
-    const handlePopState = () => {
-      setPathname(window.location.pathname)
-    }
-
-    window.addEventListener('popstate', handlePopState)
-
-    return () => window.removeEventListener('popstate', handlePopState)
-  }, [])
-
-  const currentRoute = routes.find((route) => route.path === pathname) ?? routes[0]
-  const Page = componentMap[currentRoute.componentId as keyof typeof componentMap]
-
   return (
-    <Page />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/static-form" element={<StaticForm />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- my-project/src/pages/Home.tsx
+++ my-project/src/pages/Home.tsx
@@ -1,17 +1,14 @@
-
-import React from 'react'
+import { Link } from 'react-router'
 import { Button } from '@/components/ui/button'
 
 const Home = () => {
-  function openForm() {
-    window.location.href = '/static-form'
-  }
-
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-3">Welcome to the Home page</h1>
       <p className="mb-4">Click the button below to open the static form.</p>
-      <Button onClick={openForm}>Open Static Form</Button>
+      <Button asChild>
+        <Link to="/static-form">Open Static Form</Link>
+      </Button>
     </div>
   )
 }
